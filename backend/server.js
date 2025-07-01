119978cc0050b4e78b815f7443ba6747580a4f5e
r27ohan05 <https://github.com/r27ohan05/ChatApp>
2025-07-01 12:35:57 +0530

    Initial full project with frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve the frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+}); 
